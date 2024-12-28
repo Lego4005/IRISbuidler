9e51fcf08c54176fbe041523f10b1021775f8edd
Lego4005 <colby@bit9.ai>
2024-12-27 21:05:06 -0500

    style: Fix quotes in Remix config

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
-  server: "./server.js",
-  serverBuildPath: "netlify/functions/server/index.js",
-  serverModuleFormat: "cjs",
-  serverPlatform: "node",
+  ignoredRouteFiles: ['**/.*'],
+  server: './server.js',
+  serverBuildPath: 'netlify/functions/server/index.js',
+  serverModuleFormat: 'cjs',
+  serverPlatform: 'node',
   serverMinify: false,
-  appDirectory: "app",
-  assetsBuildDirectory: "build/client",
-  publicPath: "/build/",
+  appDirectory: 'app',
+  assetsBuildDirectory: 'build/client',
+  publicPath: '/build/',
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
