ecf29cc6a8527822ab16b76e69a554ea51409bf3
Lego4005 <colby@bit9.ai>
2024-12-27 21:33:31 -0500

    fix: Simplify Vite config and update Remix dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,56 +3,30 @@ import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import * as sass from 'sass-embedded';
 import { resolve } from 'path';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
     nodePolyfills({
-      include: ['path', 'buffer', 'process'],
-      globals: {
-        process: true,
-      },
+      include: ['path', 'buffer'],
     }),
-    remixVitePlugin({
-      ssr: true,
-      serverModuleFormat: 'cjs',
-    }),
-    UnoCSS({
-      mode: 'global',
-    }),
+    remixVitePlugin(),
+    UnoCSS(),
     tsconfigPaths(),
   ],
   css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
-        sassOptions: {
-          includePaths: ['node_modules'],
-        },
-      },
-    },
     modules: {
       localsConvention: 'camelCase',
     },
-    postcss: {
-      plugins: [autoprefixer()],
+  },
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './app'),
     },
   },
   build: {
-    target: 'esnext',
     rollupOptions: {
-      external: ['path', 'virtual:uno.css?__remix_sideEffect__', 'virtual:uno.css'],
+      external: ['virtual:uno.css?__remix_sideEffect__'],
     },
-    assetsInlineLimit: 0,
   },
-  resolve: {
-    alias: [
-      { find: '~', replacement: resolve(__dirname, 'app') },
-      { find: '/icons', replacement: resolve(__dirname, 'public/icons') },
-    ],
-  },
-  publicDir: 'public',
-  base: '/',
 });
