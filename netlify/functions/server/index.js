529a711a50601942347a5d36bdae982da443493d
Lego4005 <colby@bit9.ai>
2024-12-27 23:29:39 -0500

    fix: Update Netlify configuration with recommended settings

--- netlify/functions/server/index.js
+++ netlify/functions/server/index.js
@@ -1,14 +1,7 @@
 import { createRequestHandler } from '@remix-run/netlify';
 import * as build from '@remix-run/dev/server-build';
 
-const handler = createRequestHandler({
+export const handler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
-  getLoadContext(event) {
-    return {
-      event,
-    };
-  },
 });
-
-export { handler };
