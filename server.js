10427ee326bb263dc1e9e51c2814f55c985dbb19
Lego4005 <colby@bit9.ai>
2024-12-27 20:54:46 -0500

    fix: Add Express server for Netlify deployment

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
-import { createRequestHandler } from '@remix-run/netlify';
-import * as build from '@remix-run/dev/server-build';
+const path = require('path');
+const { createRequestHandler } = require('@remix-run/express');
+const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const { installGlobals } = require('@remix-run/node');
 
-export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-  getLoadContext(event) {
-    return {
-      event,
-    };
-  },
+installGlobals();
+
+const BUILD_DIR = path.join(process.cwd(), 'build');
+
+const app = express();
+
+app.use(compression());
+app.use(morgan('tiny'));
+
+app.disable('x-powered-by');
+
+app.use(
+  '/build',
+  express.static('public/build', { immutable: true, maxAge: '1y' })
+);
+
+app.use(express.static('public', { maxAge: '1h' }));
+
+app.all(
+  '*',
+  createRequestHandler({
+    build: require(BUILD_DIR),
+    mode: process.env.NODE_ENV,
+  })
+);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
