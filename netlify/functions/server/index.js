ccd8ead8dd5b4d9a782e079270bd4e9b2fb9e17a
Lego4005 <colby@bit9.ai>
2024-12-27 23:14:40 -0500

    fix: Update server function to use ES modules

--- netlify/functions/server/index.js
+++ netlify/functions/server/index.js
@@ -1,15 +1,14 @@
-const { createRequestHandler } = require('@remix-run/netlify');
-const build = require('@remix-run/dev/server-build');
+import { createRequestHandler } from '@remix-run/netlify';
+import * as build from '@remix-run/dev/server-build';
 
 const handler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
   getLoadContext(event) {
-    // Get the context from the event
     return {
       event,
     };
   },
 });
 
-module.exports = { handler };
+export { handler };
